4283a30f66757396f17efcb53fdc3741837d40b0
Oneironaut21 <mblinow@gmail.com>
2017-04-26 22:25:26 -0400

    Finished Part 4 of the Tutorial

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -40,6 +40,9 @@ class Book(models.Model):
         Returns url to access a book instance
         """
         return reverse('book-detail', args=[str(self.id)])
+    def display_genre(self):
+        return ','.join([genre.name for genre in self.genre.all()[:3]])
+    display_genre.short_description = 'Genre'
 
 import uuid #Required for unique book instances
 
