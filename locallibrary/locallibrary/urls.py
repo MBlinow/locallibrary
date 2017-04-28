79981c56fc5d196ad84dd1c4bcdc4cfaedbfe4f0
Oneironaut21 <mblinow@gmail.com>
2017-04-27 22:27:04 -0400

    Finished part 5 of tutorial

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    #url(r'^$', views.index, name='index'),
 ]
 # Use include() to add URLS from the catalog application
 from django.conf.urls import include
